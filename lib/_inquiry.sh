7932f570cd28cebf8fdfc98d55c2da29592d1d61
Firecloud <contato@firecloud.solutions>
2023-01-29 08:53:33 -0300

    Update

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -11,7 +11,7 @@ get_mysql_root_password() {
 get_link_git() {
   
   print_banner
-  printf "${WHITE} 💻 Insira o link do GITHUB do Whaticket que deseja instalar:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Insira o link da sua instalção Conecteup que deseja instalar:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " link_git
 }
@@ -201,14 +201,14 @@ software_dominio() {
 inquiry_options() {
   
   print_banner
-  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador Whaticket, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador Conecteup, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [0] Instalar whaticket\n"
-  printf "   [1] Atualizar whaticket\n"
-  printf "   [2] Deletar Whaticket\n"
-  printf "   [3] Bloquear Whaticket\n"
-  printf "   [4] Desbloquear Whaticket\n"
-  printf "   [5] Alter. dominio Whaticket\n"
+  printf "   [0] Instalar Conecteup\n"
+  printf "   [1] Atualizar Conecteup\n"
+  printf "   [2] Deletar Conecteup\n"
+  printf "   [3] Bloquear Conecteup\n"
+  printf "   [4] Desbloquear Conecteup\n"
+  printf "   [5] Alter. dominio Conecteup\n"
   printf "\n"
   read -p "> " option
 
